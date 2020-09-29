e8eddb93ece53a23e14dd1c9b51ff0bf2332f342
Lucid-149 <peterk149@gmail.com>
2020-09-29 21:01:48 +0300

    master

---
+++ pages/Shopping.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="~/assets/img/Fashion.jpg" alt="" />
+      <li v-for="post of posts" :key="post.slug">
+        <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+      </li>
+    </div>
+  </div>
+</template>
+<script></script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="~/assets/img/Fashion.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
