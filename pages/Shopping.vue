b6dd014271e4c4549c983fc87ef0c82ec9948f19
Lucid-149 <peterk149@gmail.com>
2020-09-29 21:14:37 +0300

    save

--- pages/Shopping.vue
+++ pages/Shopping.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <img src="~/assets/img/Fashion.jpg" alt="" />
-      <li v-for="post of posts" :key="post.slug">
-        <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
-      </li>
-    </div>
+    <div>v-show="condition"</div>
   </div>
 </template>
 <script></script>
